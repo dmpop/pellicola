9ef89382f37e7d1383d230a7deaeab01c7e4117a
Dmitri Popov <dmpop@linux.com>
2013-07-22 11:57:28 +0200

    Simplified photocrumbs.py

--- photocrumbs.py
+++ photocrumbs.py
@@ -6,36 +6,25 @@ timestamp = time.strftime('%Y%m%d-%H%M%S', time.localtime())
 droid.cameraInteractiveCapturePicture('/sdcard/' + timestamp + '.jpg')
 
 notetext = droid.dialogGetInput('Note', 'Add a note to the photocrumb:').result
-note = timestamp + '.txt'
+note = timestamp + '.php'
 file = open('/sdcard/' + note, 'a')
 file.write('%s\n' % (notetext))
 file.close()
 
-server = droid.dialogGetInput('FTP Server', 'FTP server IP address:').result
-username = droid.dialogGetInput('User name', 'Enter your FTP user name:').result
-password = droid.dialogGetPassword('Password', 'Enter your FTP password:').result
+server = '127.0.0.1'
+username = 'username'
+password = 'password'
 
-droid.dialogCreateAlert('Upload the photocrumb and the note?')
-droid.dialogSetPositiveButtonText('Yes')
-droid.dialogSetNegativeButtonText('No')
+
+droid.dialogCreateSpinnerProgress('Uploading to FTP...')
 droid.dialogShow()
-response = droid.dialogGetResponse().result
-	
-if response['which'] == 'positive':
-	try:
-		droid.dialogCreateSpinnerProgress('Uploading to FTP...')
-		droid.dialogShow()
-		conn = ftplib.FTP(server, username, password)
-		file = open ('/sdcard/' + timestamp + '.jpg', 'rb')
-		conn.storbinary('STOR  ' + timestamp + '.jpg', file)
-		file.close()
-		file = open ('/sdcard/' + note, 'rb')
-		conn.storbinary('STOR ' + note, file)
-		file.close()
-		conn.quit()
-		droid.notify('Photocrumbs', 'Upload completed.')
-		droid.dialogDismiss()
-	except Exception:
-		droid.dialogCreateAlert('Upload failed.')
-		droid.dialogSetPositiveButtonText('OK')
-		droid.dialogShow()
+conn = ftplib.FTP(server, username, password)
+file = open ('/sdcard/' + timestamp + '.jpg', 'rb')
+conn.storbinary('STOR ' + timestamp + '.jpg', file)
+file.close()
+file = open ('/sdcard/' + note, 'rb')
+conn.storbinary('STOR ' + note, file)
+file.close()
+conn.quit()
+droid.notify('Photocrumbs', 'Upload completed.')
+droid.dialogDismiss()
