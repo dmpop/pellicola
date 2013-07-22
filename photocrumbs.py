22b9512f2cfb8f37f3e90f4ad5af7e794e1f982c
Dmitri Popov <dmpop@linux.com>
2013-07-22 13:43:52 +0200

    Modified photocrumbs.py

--- photocrumbs.py
+++ photocrumbs.py
@@ -18,16 +18,25 @@ server = '127.0.0.1'
 username = 'username'
 password = 'password'
 
-
-droid.dialogCreateSpinnerProgress('Uploading to FTP...')
+droid.dialogCreateAlert('Upload the photocrumb and the note?')
+droid.dialogSetPositiveButtonText('Yes')
+droid.dialogSetNegativeButtonText('No')
 droid.dialogShow()
-conn = ftplib.FTP(server, username, password)
-file = open ('/sdcard/photocrumbs/' + timestamp + '.jpg', 'rb')
-conn.storbinary('STOR ' + timestamp + '.jpg', file)
-file.close()
-file = open ('/sdcard/photocrumbs/' + note, 'rb')
-conn.storbinary('STOR ' + note, file)
-file.close()
-conn.quit()
-droid.notify('Photocrumbs', 'Upload completed.')
-droid.dialogDismiss()
+response = droid.dialogGetResponse().result
+	
+if response['which'] == 'positive':
+	droid.dialogCreateSpinnerProgress('Uploading to FTP...')
+	droid.dialogShow()
+	conn = ftplib.FTP(server, username, password)
+	file = open ('/sdcard/photocrumbs/' + timestamp + '.jpg', 'rb')
+	conn.storbinary('STOR ' + timestamp + '.jpg', file)
+	file.close()
+	file = open ('/sdcard/photocrumbs/' + note, 'rb')
+	conn.storbinary('STOR ' + note, file)
+	file.close()
+	conn.quit()
+	droid.notify('Photocrumbs', 'Upload completed.')
+	droid.dialogDismiss()
+else:
+		droid.notify('Photocrumbs', 'All done!')
+		droid.dialogDismiss()
