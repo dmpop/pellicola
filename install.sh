7ab455bae73ecf52fdbe384df52176a6745d751c
Dmitri Popov <dmpop@linux.com>
2014-07-18 14:28:05 +0200

    Photocrumbs is now Mejiro

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 apt-get update
 apt-get -y install lighttpd php5 php5-gd git
 cd /var/www/
-git clone https://github.com/dmpop/photocrumbs.git
+git clone https://github.com/dmpop/mejiro.git
 chown www-data:www-data -R mejiro/
 echo "All done!"
