a1bba955a5dca96374fe19a8fb18652b9a902c86
Dmitri Popov <dmpop@linux.com>
2014-07-22 18:01:27 +0200

    Minor tweaks

--- index.php
+++ index.php
@@ -24,50 +24,50 @@
 	// ----------------------------
 
 	/**
- * Returns an array of latitude and longitude from the Image file
- * @param image $file
- * @return multitype:number |boolean
- * http://stackoverflow.com/questions/5449282/reading-geotag-data-from-image-in-php
- */
-function read_gps_location($file){
-    if (is_file($file)) {
-        $info = exif_read_data($file);
-        if (isset($info['GPSLatitude']) && isset($info['GPSLongitude']) &&
-            isset($info['GPSLatitudeRef']) && isset($info['GPSLongitudeRef']) &&
-            in_array($info['GPSLatitudeRef'], array('E','W','N','S')) && in_array($info['GPSLongitudeRef'], array('E','W','N','S'))) {
+	* Returns an array of latitude and longitude from the Image file
+	* @param image $file
+	* @return multitype:number |boolean
+	* http://stackoverflow.com/questions/5449282/reading-geotag-data-from-image-in-php
+	*/
+	function read_gps_location($file){
+	if (is_file($file)) {
+		$info = exif_read_data($file);
+		if (isset($info['GPSLatitude']) && isset($info['GPSLongitude']) &&
+			isset($info['GPSLatitudeRef']) && isset($info['GPSLongitudeRef']) &&
+			in_array($info['GPSLatitudeRef'], array('E','W','N','S')) && in_array($info['GPSLongitudeRef'], array('E','W','N','S'))) {
 
-            $GPSLatitudeRef  = strtolower(trim($info['GPSLatitudeRef']));
-            $GPSLongitudeRef = strtolower(trim($info['GPSLongitudeRef']));
+			$GPSLatitudeRef  = strtolower(trim($info['GPSLatitudeRef']));
+			$GPSLongitudeRef = strtolower(trim($info['GPSLongitudeRef']));
 
-            $lat_degrees_a = explode('/',$info['GPSLatitude'][0]);
-            $lat_minutes_a = explode('/',$info['GPSLatitude'][1]);
-            $lat_seconds_a = explode('/',$info['GPSLatitude'][2]);
-            $lng_degrees_a = explode('/',$info['GPSLongitude'][0]);
-            $lng_minutes_a = explode('/',$info['GPSLongitude'][1]);
-            $lng_seconds_a = explode('/',$info['GPSLongitude'][2]);
+			$lat_degrees_a = explode('/',$info['GPSLatitude'][0]);
+			$lat_minutes_a = explode('/',$info['GPSLatitude'][1]);
+			$lat_seconds_a = explode('/',$info['GPSLatitude'][2]);
+			$lng_degrees_a = explode('/',$info['GPSLongitude'][0]);
+			$lng_minutes_a = explode('/',$info['GPSLongitude'][1]);
+			$lng_seconds_a = explode('/',$info['GPSLongitude'][2]);
 
-            $lat_degrees = $lat_degrees_a[0] / $lat_degrees_a[1];
-            $lat_minutes = $lat_minutes_a[0] / $lat_minutes_a[1];
-            $lat_seconds = $lat_seconds_a[0] / $lat_seconds_a[1];
-            $lng_degrees = $lng_degrees_a[0] / $lng_degrees_a[1];
-            $lng_minutes = $lng_minutes_a[0] / $lng_minutes_a[1];
-            $lng_seconds = $lng_seconds_a[0] / $lng_seconds_a[1];
+			$lat_degrees = $lat_degrees_a[0] / $lat_degrees_a[1];
+			$lat_minutes = $lat_minutes_a[0] / $lat_minutes_a[1];
+			$lat_seconds = $lat_seconds_a[0] / $lat_seconds_a[1];
+			$lng_degrees = $lng_degrees_a[0] / $lng_degrees_a[1];
+			$lng_minutes = $lng_minutes_a[0] / $lng_minutes_a[1];
+			$lng_seconds = $lng_seconds_a[0] / $lng_seconds_a[1];
 
-            $lat = (float) $lat_degrees+((($lat_minutes*60)+($lat_seconds))/3600);
-            $lng = (float) $lng_degrees+((($lng_minutes*60)+($lng_seconds))/3600);
+			$lat = (float) $lat_degrees+((($lat_minutes*60)+($lat_seconds))/3600);
+			$lng = (float) $lng_degrees+((($lng_minutes*60)+($lng_seconds))/3600);
 
-            //If the latitude is South, make it negative.
-            //If the longitude is west, make it negative
-            $GPSLatitudeRef  == 's' ? $lat *= -1 : '';
-            $GPSLongitudeRef == 'w' ? $lng *= -1 : '';
+			//If the latitude is South, make it negative.
+			//If the longitude is west, make it negative
+			$GPSLatitudeRef  == 's' ? $lat *= -1 : '';
+			$GPSLongitudeRef == 'w' ? $lng *= -1 : '';
 
-            return array(
-                'lat' => $lat,
-                'long' => $lng
-            );
-        }
-    }
-    return false;
+			return array(
+				'lat' => $lat,
+				'long' => $lng
+			);
+		}
+	}
+	return false;
 }
 
 	// Create the required directories if they don't exist
