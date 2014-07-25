17ca0d8c6bae4b3692e5bf79fcd6b1881521251b
Dmitri Popov <dmpop@linux.com>
2014-07-25 14:18:34 +0200

    Rebuild thumbnails feature added. Minor tweaks.

--- index.php
+++ index.php
@@ -24,50 +24,50 @@
 	// ----------------------------
 
 	/**
-	* Returns an array of latitude and longitude from the Image file
-	* @param image $file
-	* @return multitype:number |boolean
-	* http://stackoverflow.com/questions/5449282/reading-geotag-data-from-image-in-php
-	*/
-	function read_gps_location($file){
-	if (is_file($file)) {
-		$info = exif_read_data($file);
-		if (isset($info['GPSLatitude']) && isset($info['GPSLongitude']) &&
-			isset($info['GPSLatitudeRef']) && isset($info['GPSLongitudeRef']) &&
-			in_array($info['GPSLatitudeRef'], array('E','W','N','S')) && in_array($info['GPSLongitudeRef'], array('E','W','N','S'))) {
+ * Returns an array of latitude and longitude from the Image file
+ * @param image $file
+ * @return multitype:number |boolean
+ * http://stackoverflow.com/questions/5449282/reading-geotag-data-from-image-in-php
+ */
+function read_gps_location($file){
+    if (is_file($file)) {
+        $info = exif_read_data($file);
+        if (isset($info['GPSLatitude']) && isset($info['GPSLongitude']) &&
+            isset($info['GPSLatitudeRef']) && isset($info['GPSLongitudeRef']) &&
+            in_array($info['GPSLatitudeRef'], array('E','W','N','S')) && in_array($info['GPSLongitudeRef'], array('E','W','N','S'))) {
 
-			$GPSLatitudeRef = strtolower(trim($info['GPSLatitudeRef']));
-			$GPSLongitudeRef = strtolower(trim($info['GPSLongitudeRef']));
+            $GPSLatitudeRef  = strtolower(trim($info['GPSLatitudeRef']));
+            $GPSLongitudeRef = strtolower(trim($info['GPSLongitudeRef']));
 
-			$lat_degrees_a = explode('/',$info['GPSLatitude'][0]);
-			$lat_minutes_a = explode('/',$info['GPSLatitude'][1]);
-			$lat_seconds_a = explode('/',$info['GPSLatitude'][2]);
-			$lng_degrees_a = explode('/',$info['GPSLongitude'][0]);
-			$lng_minutes_a = explode('/',$info['GPSLongitude'][1]);
-			$lng_seconds_a = explode('/',$info['GPSLongitude'][2]);
+            $lat_degrees_a = explode('/',$info['GPSLatitude'][0]);
+            $lat_minutes_a = explode('/',$info['GPSLatitude'][1]);
+            $lat_seconds_a = explode('/',$info['GPSLatitude'][2]);
+            $lng_degrees_a = explode('/',$info['GPSLongitude'][0]);
+            $lng_minutes_a = explode('/',$info['GPSLongitude'][1]);
+            $lng_seconds_a = explode('/',$info['GPSLongitude'][2]);
 
-			$lat_degrees = $lat_degrees_a[0] / $lat_degrees_a[1];
-			$lat_minutes = $lat_minutes_a[0] / $lat_minutes_a[1];
-			$lat_seconds = $lat_seconds_a[0] / $lat_seconds_a[1];
-			$lng_degrees = $lng_degrees_a[0] / $lng_degrees_a[1];
-			$lng_minutes = $lng_minutes_a[0] / $lng_minutes_a[1];
-			$lng_seconds = $lng_seconds_a[0] / $lng_seconds_a[1];
+            $lat_degrees = $lat_degrees_a[0] / $lat_degrees_a[1];
+            $lat_minutes = $lat_minutes_a[0] / $lat_minutes_a[1];
+            $lat_seconds = $lat_seconds_a[0] / $lat_seconds_a[1];
+            $lng_degrees = $lng_degrees_a[0] / $lng_degrees_a[1];
+            $lng_minutes = $lng_minutes_a[0] / $lng_minutes_a[1];
+            $lng_seconds = $lng_seconds_a[0] / $lng_seconds_a[1];
 
-			$lat = (float) $lat_degrees+((($lat_minutes*60)+($lat_seconds))/3600);
-			$lng = (float) $lng_degrees+((($lng_minutes*60)+($lng_seconds))/3600);
+            $lat = (float) $lat_degrees+((($lat_minutes*60)+($lat_seconds))/3600);
+            $lng = (float) $lng_degrees+((($lng_minutes*60)+($lng_seconds))/3600);
 
-			//If the latitude is South, make it negative.
-			//If the longitude is west, make it negative
-			$GPSLatitudeRef == 's' ? $lat *= -1 : '';
-			$GPSLongitudeRef == 'w' ? $lng *= -1 : '';
+            //If the latitude is South, make it negative.
+            //If the longitude is west, make it negative
+            $GPSLatitudeRef  == 's' ? $lat *= -1 : '';
+            $GPSLongitudeRef == 'w' ? $lng *= -1 : '';
 
-			return array(
-				'lat' => $lat,
-				'long' => $lng
-			);
-		}
-	}
-	return false;
+            return array(
+                'lat' => $lat,
+                'long' => $lng
+            );
+        }
+    }
+    return false;
 }
 
 	// Create the required directories if they don't exist
@@ -93,7 +93,7 @@
 		$height = imagesy($img);
 
 		// calculate thumbnail size
-		$new_width = $thumbWidth;
+		$new_width  = $thumbWidth;
 		$new_height = floor($height * ($thumbWidth / $width));
 
 		// create a new temporary image
@@ -115,7 +115,7 @@
 
 	// Generate any missing thumbnails and check expiration
 	for($i = 0; $i < $fileCount; $i++) {
-		$file = $files[$i];
+		$file  = $files[$i];
 		$thumb = "photos/thumbs/".basename($file);
 
 		if(!file_exists($thumb)) {
@@ -143,16 +143,26 @@
 	echo "<body>";
 	echo "<div id='content'>";
 
+	// The $r parameter is used to clear the photos/thumbs directory
+	$rebuild = $_GET['r'];
+	if (isset($rebuild)) {
+		$files = glob('photos/thumbs/*');
+			foreach($files as $file){
+				unlink($file);
+			}
+		exit("Thumbnails have been deleted. <a href='".basename($_SERVER['PHP_SELF'])."'>Reload the page</a> to rebuild thumbnails.");
+		}
+
 	// The $t parameter is used to hide the thumbnails
 	$view = $_GET['t'];
-	if (empty($view)) {
+	if (!isset($view)) {
 		echo "<h1>".$title."</h1>";
 		echo "<p>";
 		for ($i=($fileCount-1); $i>=0; $i--) {
 			$file = $files[$i];
 			$thumb = "photos/thumbs/".basename($file);
 			$filepath = pathinfo($file);
-			echo '<a href="index.php?p='.$file.'&t=1"><img src="'.$thumb.'" alt="'.$filepath['filename'].'" title="'.$filepath['filename'].'" width=128 hspace="1"></a>';
+			echo '<a href="index.php?p='.$file.'&t"><img src="'.$thumb.'" alt="'.$filepath['filename'].'" title="'.$filepath['filename'].'" width=128 hspace="1"></a>';
 		}
 	}
 
@@ -209,7 +219,7 @@
 		$ip=$_SERVER['REMOTE_ADDR'];
 		$date = $date = date('Y-m-d H:i:s');
 		$file = fopen("ip.log", "a+");
-		fputs($file, " $ip    $page    $date\n");
+		fputs($file, " $ip  $page $date \n");
 		fclose($file);
 	}
 
