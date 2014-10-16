1fbc293edfae1f814d871038cd334a00bbece25d
Dmitri Popov <dmpop@linux.com>
2014-10-16 16:15:16 +0200

    thinkpad-t410-kubuntu

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get -y install lighttpd php5 php5-gd git
+apt-get -y install apache2 php5 php5-gd git
 cd /var/www/
 git clone https://github.com/dmpop/mejiro.git
 chown www-data:www-data -R mejiro/
