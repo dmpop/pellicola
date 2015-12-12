0bc7ac81037a1f720de1019a67b5af834d14ff36
Dmitri Popov <dmpop@linux.com>
2015-12-12 09:18:44 +0100

    Better redirects and page refreshes :monkey:

--- index.php
+++ index.php
@@ -62,13 +62,13 @@
 	<a href="https://github.com/dmpop/mejiro" class="github-corner"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:#64CEAA; color:#fff; position: absolute; top: 0; border: 0; left: 0; transform: scale(-1, 1);"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a><style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>
 
 	<?php
-	//Suppress all error messages.
+	// Suppress all error messages.
 	//error_reporting (E_ALL ^ E_NOTICE);
 
 	// Detect browser language.
 	$language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 	
-	// The $d parameter is used to detect a sub-directory.
+	// The $d parameter is used to detect a subdirectory.
 	$sub_photo_dir = (isset($_GET['d']) ? $_GET['d'] : null);
 	if (isset($sub_photo_dir)) {
 		$photo_dir = $photo_dir.$sub_photo_dir.'/';
@@ -200,14 +200,14 @@
 	echo "<body>";
 	echo "<div id='content'>";
 
-	// The $rebuild parameter is used to empty the $photo_dir.tims directory.
+	// The $rebuild parameter is used to empty the tims directory.
 	$rm_tim = (isset($_GET['rebuild']) ? $_GET['rebuild'] : null);
 	if (isset($rm_tim)) {
 		$files = glob($photo_dir.'tims/*');
 			foreach($files as $file){
 				unlink($file);
 			}
-		exit("Tims have been deleted. <a href='".basename($_SERVER['PHP_SELF'])."'>Reload the page</a> to rebuild tims.");
+		header('Location: '.$_SERVER['PHP_SELF']."?d=".$sub_photo_dir);
 		}
 
 	// The $grid parameter is used to show the main grid.
@@ -335,24 +335,24 @@
 	}
 
 	// The $menu parameter is used to show the menu.
-	$help = (isset($_GET['menu']) ? $_GET['menu'] : null);
-	if (isset($help)) {
-		echo '<p class="box"><a href="'.$_SERVER['PHP_SELF'].'?rebuild"><i class="fa fa-wrench fa-lg"></i></a> Rebuild tims --- <a href="'.$_SERVER['PHP_SELF'].'?upload"><i class="fa fa-upload fa-lg"></i></a> Show upload form --- <a href="'.$_SERVER['PHP_SELF'].'"><i class="fa fa-times fa-lg"></i></a> Close menu</p>';
+	$menu = (isset($_GET['menu']) ? $_GET['menu'] : null);
+	if (isset($menu)) {
+		echo '<p class="box"><a href="'.$_SERVER['PHP_SELF'].'?rebuild&d='.$sub_photo_dir.'"><i class="fa fa-wrench fa-lg"></i></a> Rebuild tims --- <a href="'.$_SERVER['PHP_SELF'].'?upload&d='.$sub_photo_dir.'"><i class="fa fa-upload fa-lg"></i></a> Show upload form --- <a href="'.$_SERVER['PHP_SELF'].'?d='.$sub_photo_dir.'"><i class="fa fa-times fa-lg"></i></a> Close menu</p>';
 	}
 
 	// Upload form adapted from http://sebsauvage.net/wiki/doku.php?id=php:filehosting
 	// The $upload parameter is used to show the upload form.
 	$upload = (isset($_GET['upload']) ? $_GET['upload'] : null);
 	if (isset($upload)) {
-	$scriptname = basename($_SERVER["SCRIPT_NAME"]).'?upload';
+	$scriptname = basename($_SERVER["SCRIPT_NAME"]).'?upload&d='.$sub_photo_dir;
 	if (isset($_FILES['filetoupload']) && isset($_POST['password'])){
 			sleep(3); // Reduce brute-force attack effectiveness
-		if ($_POST['password']!=$password) { print '<br /><p class="box">Wrong password! <a href="'.basename($_SERVER['PHP_SELF']).'">Back</a></p>'; header($_SERVER['PHP_SELF']); exit(); }
+		if ($_POST['password']!=$password) { print '<br /><p class="box">Wrong password! <a href="'.basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir.'">Back</a></p>'; header($_SERVER['PHP_SELF']); exit(); }
 			$filename = $photo_dir.basename( $_FILES['filetoupload']['name']);
 			if (file_exists($filename)) { print '<br /><p class="box">This file already exists. <a href="'.basename($_SERVER['PHP_SELF']).'">Back</a></p>'; header($_SERVER['PHP_SELF']); exit(); }
 			if(move_uploaded_file($_FILES['filetoupload']['tmp_name'], $filename)){ $serverport=''; if ($_SERVER["SERVER_PORT"]!='80') { $serverport=':'.$_SERVER["SERVER_PORT"]; }
 	$fileurl='http://'.$_SERVER["SERVER_NAME"].$serverport.dirname($_SERVER["SCRIPT_NAME"]).$photo_dir.basename($_FILES['filetoupload']['name']);
-	print '<br /><p class="box">Upload successful. <a href="'.basename($_SERVER['PHP_SELF']).'">Reload</a> the page to finish.</p>';
+	header('Location: '.$_SERVER['PHP_SELF']."?d=".$sub_photo_dir);
 	}
 	else { echo '<br /><p class="box">There was an error uploading the file, please try again!</p>'; }
 	}
@@ -364,7 +364,7 @@
 EOD;
 }
 
-	echo '<div class="footer">'.$footer.' <a href="'.$_SERVER['PHP_SELF'].'?menu"><i class="fa fa-cogs fa-lg"></i></a></div>';
+	echo '<div class="footer">'.$footer.' <a href="'.$_SERVER['PHP_SELF'].'?menu&d='.$sub_photo_dir.'"><i class="fa fa-cogs fa-lg"></i></a></div>';
 
 	if ($stats) {
 	echo '<p class="center">';
