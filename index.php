ed6a4e212d8b2538253bb210511a794ed1bf2f54
Dmitri Popov <dmpop@linux.com>
2016-02-17 00:29:19 +0100

    Added link-based navigation :monkey:

--- index.php
+++ index.php
@@ -302,19 +302,19 @@
 
 		// If there is only one photo in the album, show the home navigation tim.
 		if ($fileCount == 1) {
-                    echo "<p class='center'><a href='".basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir."' accesskey='h'><img class='tim' style='-webkit-border-radius: 9px; -moz-border-radius: 9px; border-radius: 9px;' src=".$photo_dir."tims/".basename(max($files))."></a></p>";
+                    echo "<p class='center'><a href='".basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir."' accesskey='h'>Grid</a> &bull; </p>";
 		}
 		// Disable the Next link if this is the last photo.
 		elseif (empty($files[$key+1])) {
-                    echo "<p class='center'><a href='".basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir."' accesskey='h'><img class='tim' style='-webkit-border-radius: 9px; -moz-border-radius: 9px; border-radius: 9px; padding-right:10px;' src=".$photo_dir."tims/".basename(max($files))."></a><a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key-1].'&d='.$sub_photo_dir."' accesskey='p'><img class='tim' src=".$photo_dir."tims/".basename($files[$key-1])."></a></a><a href='".basename($_SERVER['PHP_SELF'])."?photo=".min($files).'&d='.$sub_photo_dir."' accesskey='h'><img class='tim' style='-webkit-border-radius: 9px; -moz-border-radius: 9px; border-radius: 9px; padding-left:9px;' src=".$photo_dir."tims/".basename(min($files))."></p>";
+                    echo "<p class='center'><a href='".basename($_SERVER['PHP_SELF']).'?d='.$sub_photo_dir."' accesskey='h'>Grid</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key-1].'&d='.$sub_photo_dir."' accesskey='p'>Previous</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".min($files).'&d='.$sub_photo_dir."' accesskey='h'>Last</a></p>";
 		}
 		// Disable the Previous link if this is the first photo.
 		elseif (empty($files[$key-1])) {
-                    echo "<p class='center'><a href='".basename($_SERVER['PHP_SELF'])."?photo=".max($files).'&d='.$sub_photo_dir."' accesskey='h'><img class='tim' style='-webkit-border-radius: 9px; -moz-border-radius: 9px; border-radius: 9px; padding-right:10px;' src=".$photo_dir."tims/".basename(max($files))."></a><a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key+1].'&d='.$sub_photo_dir."' accesskey='n'><img class='tim' src=".$photo_dir."tims/".basename($files[$key+1])."></a></p>";
+                    echo "<p class='center'><a href='".basename($_SERVER['PHP_SELF'])."?photo=".max($files).'&d='.$sub_photo_dir."' accesskey='h'>First</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key+1].'&d='.$sub_photo_dir."' accesskey='n'>Next</a></p>";
 		}
 		// Show all navigation tims.
 		else {
-                    echo "<p class='center'><a href='".basename($_SERVER['PHP_SELF'])."?photo=".max($files).'&d='.$sub_photo_dir."' accesskey='h'><img class='tim' style='-webkit-border-radius: 9px; -moz-border-radius: 9px; border-radius: 9px; padding-right:10px;' src=".$photo_dir."tims/".basename(max($files))."></a><a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key+1].'&d='.$sub_photo_dir."' accesskey='n'><img class='tim' src=".$photo_dir."tims/".basename($files[$key+1]).'?d='.$sub_photo_dir."></a><a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key-1].'&d='.$sub_photo_dir."' accesskey='p'><img class='tim' style='padding-left:9px;' src=".$photo_dir."tims/".basename($files[$key-1])."></a><a href='".basename($_SERVER['PHP_SELF'])."?photo=".min($files).'&d='.$sub_photo_dir."' accesskey='h'><img class='tim' style='-webkit-border-radius: 9px; -moz-border-radius: 9px; border-radius: 9px; padding-left:9px;' src=".$photo_dir."tims/".basename(min($files))."></a></p>";
+                    echo "<p class='center'><a href='".basename($_SERVER['PHP_SELF'])."?photo=".max($files).'&d='.$sub_photo_dir."' accesskey='h'>First</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key+1].'&d='.$sub_photo_dir."' accesskey='n'>Next</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".$files[$key-1].'&d='.$sub_photo_dir."' accesskey='p'>Previous</a> &bull; <a href='".basename($_SERVER['PHP_SELF'])."?photo=".min($files).'&d='.$sub_photo_dir."'>Last</a></p>";
 		}
 	}
 	
