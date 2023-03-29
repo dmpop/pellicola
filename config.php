2d1c61d3c290b1b0db84858ef8286864d6273ffe
Dmitri Popov <dmpop@linux.com>
2023-03-29 12:10:29 +0200

    Reorder config options

--- config.php
+++ config.php
@@ -1,19 +1,19 @@
 <?php
 $title = "Mejiro";
 $subtitle = "Responsive open-source photo grid";
-$per_page = 12; // Number of images per page for pagination
-$footer = "<a style='color: white' href='https://cameracode.coffee'>Camera, code, coffee</a>";
 $base_photo_dir = "photos"; // Directory for storing photos
-$r_sort = false;	// Set to true to show tims in the reverse order (oldest ot newest)
-$links = true;	// Enable the link box
-// If the link box is enabled, specify the desired URLs
+$per_page = 12; // Number of images per page for pagination
+$r_sort = false; // Set to true to show photos in the reversed order (oldest ot newest)
+$footer = "<a style='color: white' href='https://cameracode.coffee'>Camera, code, coffee</a>";
+$links = true;	// Enable links in the footer
+// If $links is set to true, specify the desired URLs
 $urls = array(
     array('https://github.com/dmpop/mejiro', 'Mejiro'),
     array('https://www.paypal.com/paypalme/dmpop', 'I 🧡 coffee')
 );
+$img_formats = 'jpg,jpeg,JPG,JPEG'; // File types Mejiro show display. Add other file extensions (for example, PNG, HEIC, etc.), if needed.
+$show_raw = true; // Toggle links to RAW files
+$raw_formats = 'ARW,arw,NEF,nef,ORF,orf,DNG,dng'; // Supported RAW formats. Add other file extensions, if needed
 $download = false; // Toggle photo download
-$raw_formats = 'ARW,arw,NEF,nef,ORF,orf,CR2,cr2,DNG,dng'; // Supported RAW formats. Add other formats, if needed
-$img_formats = 'jpg,jpeg,JPG,JPEG';
-$show_raw = true; // Display links to RAW files
 $goatcounter = false; // Toggle integration with GoatCounter web analytics
 $goatcounter_code = "helloxyz"; // Unique GoatCounter code
