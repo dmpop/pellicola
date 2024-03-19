283557ab7a02ac96b169c2709ac2062e60b907b9
Dmitri Popov <me@dmpop.xyz>
2024-03-19 15:23:54 +0100

    Another GPS fix

--- index.php
+++ index.php
@@ -98,22 +98,24 @@ if (session_status() == PHP_SESSION_NONE) {
 	   */
 	function gps($coordinate, $hemisphere)
 	{
-		if (is_string($coordinate)) {
-			$coordinate = array_map('trim', explode(',', $coordinate));
-		}
-		for ($i = 0; $i < 3; $i++) {
-			$part = explode('/', $coordinate[$i]);
-			if (count($part) == 1) {
-				$coordinate[$i] = $part[0];
-			} else if (count($part) == 2) {
-				$coordinate[$i] = floatval($part[0]) / floatval($part[1]);
-			} else {
-				$coordinate[$i] = 0;
+		if (!empty($coordinate) && !empty($hemisphere)) {
+			if (is_string($coordinate)) {
+				$coordinate = array_map('trim', explode(',', $coordinate));
 			}
+			for ($i = 0; $i < 3; $i++) {
+				$part = explode('/', $coordinate[$i]);
+				if (count($part) == 1) {
+					$coordinate[$i] = $part[0];
+				} else if (count($part) == 2) {
+					$coordinate[$i] = floatval($part[0]) / floatval($part[1]);
+				} else {
+					$coordinate[$i] = 0;
+				}
+			}
+			list($degrees, $minutes, $seconds) = $coordinate;
+			$sign = ($hemisphere == 'W' || $hemisphere == 'S') ? -1 : 1;
+			return $sign * ($degrees + $minutes / 60 + $seconds / 3600);
 		}
-		list($degrees, $minutes, $seconds) = $coordinate;
-		$sign = ($hemisphere == 'W' || $hemisphere == 'S') ? -1 : 1;
-		return $sign * ($degrees + $minutes / 60 + $seconds / 3600);
 	}
 	/* EXTRACT LATITUDE AND LONGITUDE ---END--- */
 
