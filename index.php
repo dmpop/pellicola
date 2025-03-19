7e80574bbbebadca0a6e751cff8b2647a86fb076
Dmitri Popov <me@dmpop.xyz>
2025-03-19 14:34:25 +0100

    Minor fixes

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ if (isset($_GET['nocount'])) {
 	if (isset($_GET['album'])) {
 		$_SESSION['album'] = htmlentities($_GET['album']);
 		$album = $_SESSION['album'];
-	} elseif (isset($_GET['album']) || !empty($_SESSION['album'])) {
+	} elseif (!empty($_SESSION['album'])) {
 		$album = $_SESSION['album'];
 	} else {
 		$album = NULL;
